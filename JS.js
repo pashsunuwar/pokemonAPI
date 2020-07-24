a908d77dadd2986b175551035481a541dfcfb6a3
princ31989 <nilay1989@me.com>
2020-07-24 12:09:04 +0100

    updated

--- JS.js
+++ JS.js
@@ -1,38 +1,53 @@
-// const myURL = 'https://pokeapi.co/api/v2/ability/';
-// fetch(myURL)
-// .then(
-//   function(response) {
-//     if (response.status !== 200) {
-//       console.log('Looks like there was a problem. Status Code: ' +
-//         response.status);
-//       return;
-//     }
-//     response.json().then(function(data){
-//       console.log(data);
-//     });
-//   }
-// )
-// .catch(function(err){
-//   console.log("Fetch went wrong");
-// })
 
-function getTodos() {
-  const myURL = 'https://pokeapi.co/api/v2/ability/';
-  fetch(myURL).then
-  REQ.onload = () => {
-    if (REQ.status === 200) {
-      console.log(REQ.response);
-      console.log(REQ.response.title);
-      for (let i = 0; i < 200; i++) {
-        document.querySelector("#resp").innerHTML = REQ.response.results[i].name;
-        console.log((document.querySelector("#resp").innerHTML = REQ.response[i].userId));
-        console.log((document.querySelector("#resp").innerHTML = REQ.response[i].id));
-        console.log((document.querySelector("#resp").innerHTML = REQ.response[i].title));
-        console.log((document.querySelector("#resp").innerHTML = REQ.response[i].completed));
-      }
+function getData(){
+const myURL = 'https://pokeapi.co/api/v2/ability/';
+fetch(myURL)
+.then(
+  function(response) {
+    if (response.status !== 200) {
+      console.log('Looks like there was a problem. Status Code: ' +
+        response.status);
+      return;
     }
+    response.json().then(function(data){
+      console.log(data);
+      for (let i = 1; i < 1000; i++) {
+        console.log(data.results[i].name);
+        console.log(data.results[i].url);
+
+        let para = document.createElement("P"); // Create a <p> element
+                para.className = "alert alert-danger col-md-8";
+                para.innerText = `The name is ${data.results[i].name}, \n the url is
+                  ${data.results[i].url}`; // Insert text
+                let myDiv = document.getElementById("todos");
+                myDiv.appendChild(para);
+      }
+      
+
+    });
   }
-}   
+)
+.catch(function(err){
+  console.log("Fetch went wrong");
+})
+}
+// function getTodos() {
+//   const myURL = 'https://pokeapi.co/api/v2/ability/';
+//   fetch(myURL).then
+//   myURL.onload = () => {
+//     if (myURL.status === 200) {
+//       console.log(myURL.response);
+//       console.log(myURL.response.title);
+//       for (let i = 0; i < 1000; i++) {
+//         document.querySelector("#resp").innerHTML = myURL.response.results[i].name;
+//         console.log((document.querySelector("#resp").innerHTML = REQ.response[i].userId));
+//         console.log((document.querySelector("#resp").innerHTML = REQ.response[i].id));
+//         console.log((document.querySelector("#resp").innerHTML = REQ.response[i].title));
+//         console.log((document.querySelector("#resp").innerHTML = REQ.response[i].completed));
+//       }
+//     }
+//   }
+// }   
     // Examine the text in the response
 //     response.json().then(function(data) {
 //       console.log(data);
